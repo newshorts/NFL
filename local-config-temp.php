32ae24f5f5be0b7b26d9793dd8a454544b40fcb7
Mike Newell <mike@iwearshorts.com>
2012-10-01 16:01:47 -0700

    changes to local-config.php file

--- local-config-temp.php
+++ local-config-temp.php
@@ -2,9 +2,9 @@
 
 /*
  *  
- *  UPDATE wp_options SET option_value = replace(option_value, 'http://sfsuperbowl.com', 'http://localhost.com') WHERE option_name = 'home' OR option_name = 'siteurl';
- *  UPDATE wp_posts SET guid = replace(guid, 'http://sfsuperbowl.com','http://localhost.com');
- *  UPDATE wp_posts SET post_content = replace(post_content, 'http://sfsuperbowl.com', 'http://localhost.com');
+ *  UPDATE wp_options SET option_value = replace(option_value, 'http://sfsuperbowl.com', 'http://localhost.com/GSP/clients/NFL') WHERE option_name = 'home' OR option_name = 'siteurl';
+ *  UPDATE wp_posts SET guid = replace(guid, 'http://sfsuperbowl.com','http://localhost.com/GSP/clients/NFL');
+ *  UPDATE wp_posts SET post_content = replace(post_content, 'http://sfsuperbowl.com', 'http://localhost.com/GSP/clients/NFL');
  * 
  *  SSH:                    ssh bowl_admin@72.47.248.81
  *  P:                      amfootball
