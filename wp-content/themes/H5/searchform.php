3a681518f31dc00095db2eec0d7fd20d9d329159
Mike Newell <mike@iwearshorts.com>
2012-10-09 10:51:52 -0700

    removed wordpress, because we don't want it for this project

--- wp-content/themes/H5/searchform.php
+++
@@ -1,5 +0,0 @@
-					<form method="get" action="<?php bloginfo('url'); ?>/">
-						<label for="s">Search the site: </label>
-						<input type="text" id="s" name="s" value="<?php the_search_query(); ?>">
-						<input type="submit" value="Search">
-					</form>
