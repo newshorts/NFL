5f9a4a4e4ca9f42a9331473eb69a96dceaa54bc2
Mike Newell <mike@iwearshorts.com>
2012-10-09 15:09:45 -0700

    facebook-posts

---
+++ prototypes/get-facebook-posts/get.php
@@ -0,0 +1,16 @@
+<?php
+
+require './src/facebook.php';
+
+$facebook = new Facebook(array(
+  'appId'  => '528131467203710',
+  'secret' => '049695b0d8441e9c1e9e8d78bacb1e1a',
+));
+
+// Get User ID
+$user = $facebook->getUser();
+
+echo "<pre>";
+print_r($user);
+
+?>
