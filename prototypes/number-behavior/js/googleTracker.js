ad69b29cd03814a308a5772ff8fcd9c0c9d7cdf8
Mike Newell <mike@iwearshorts.com>
2012-10-10 19:17:50 -0700

    revision of the number formatter class

--- prototypes/number-behavior/js/googleTracker.js
+++ prototypes/number-behavior/js/googleTracker.js
@@ -6,81 +6,79 @@
 
 var GoogleTracker = SocialTracker.extend({
     
-    _elem: {},
-    name: 'google_data',
-    _intervalObj: {},
-    count: 0,
-    delta: 0,
+//    _elem: {},
+//    name: 'google_data',
+//    _intervalObj: {},
+//    count: 0,
+//    delta: 0,
     
     init: function(elem) {
         
-        this._super(this.name, 'get/output.json');
+        this._super('google_data', 'get/output.json', elem);
         
         var self = this;
         
-        window.addEventListener(this.name, function(evt) {
-            console.dir(evt.detail.data.output)
+        window.addEventListener('google_data', function(evt) {
+//            console.dir(evt.detail.data.output)
             self.update(parseInt(evt.detail.data.output.google));
         }, false);
         
-        this._elem = $(elem);
-    },
-    
-    // evt.detail.data.google
-    update: function(newNum) {
-        console.dir(newNum);
-        
-        // use the name to search for the last number and store it
-        var oldNum = parseInt(this._elem.attr(this.name));
-        
-        // set the new number
-        this._elem.attr(this.name, newNum);
-        
-        // get the abs(difference) of the new and old number
-        this.delta = Math.abs(oldNum - newNum);
-        
-        console.log('old: ' + oldNum + ' - new: ' + newNum + ' = ' + this.delta);
-        
-        // if the numbers have a balance, calculate the interval (5000/delta)
-        if(this.delta > 0) {
-            var interval = Math.floor(10000/this.delta);
-            console.log(interval)
-
-            // set interval and reset count to 0
-            this.count = 0;
-            this.setTimer(interval, oldNum);
-        }
-
-        if(this.delta == 0){
-
-        }
-
-    },
-
-    setTimer: function(interval, oldNum) {
-        var self = this;
-
-        this._intervalObj = window.setInterval(function() {
-            // set the text and increment the count
-//            var num = parseInt(self._elem.attr(self.name));
-            self.count++;
-            var formattedNum = self.formatNumber(oldNum + self.count);
-            self._elem.text(formattedNum);
-            
-            console.log('count: ' + self.count)
-            console.log('delta: ' + self.delta)
-            // if the (count === delta) destroy the interval and stop
-            if(self.count > self.delta) {
-                self.destroyTimer();
-            }
-        }, interval);
-
-    },
-
-    destroyTimer: function() {
-        var self = this;
-        self.count = 0;
-        window.clearInterval(self._intervalObj);
+//        this._elem = $(elem);
     }
+    
+//    update: function(newNum) {
+//        
+//        var oldNum = parseInt(this._elem.attr(this.name));
+//        
+//        this.setAttribute(newNum);
+//        
+//        this.delta = Math.abs(oldNum - newNum);
+//        
+//        this.monitorChange(oldNum);
+//        
+//    },
+    
+//    setAttribute: function(newNum) {
+//        this._elem.attr(this.name, newNum);
+//    },
+    
+//    monitorChange: function(oldNum) {
+//        if(this.delta > 0) {
+//            var interval = Math.floor(10000/this.delta);
+//
+//            this.count = 0;
+//            this.setTimer(interval, oldNum);
+//        }
+//    },
+//    
+//    setTimer: function(interval, oldNum) {
+//        var self = this;
+//
+//        this._intervalObj = window.setInterval(function() {
+//            // set the text and increment the count
+////            var num = parseInt(self._elem.attr(self.name));
+//            self.count++;
+//            var formattedNum = self.formatNumber(oldNum + self.count);
+//            self.setText(formattedNum);
+//            
+//            console.log('count: ' + self.count)
+//            console.log('delta: ' + self.delta)
+//            // if the (count === delta) destroy the interval and stop
+//            if(self.count > self.delta) {
+//                self.destroyTimer();
+//            }
+//        }, interval);
+//
+//    },
+//    
+//    setText: function(str) {
+//        this._elem.text(str);
+//    },
+//
+//    destroyTimer: function() {
+//        var self = this;
+//        self.count = 0;
+//        window.clearInterval(self._intervalObj);
+//    }
     
 });
