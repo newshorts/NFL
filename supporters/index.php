9479e6985821fac7f5c5f1ad4766b7e35a474fcc
Russell Shearer <rgipsons@gmail.com>
2012-10-11 21:49:32 -0700

    adding styles to supporters.php

--- supporters/index.php
+++ supporters/index.php
@@ -3,53 +3,55 @@
 
 <div id="dynamic">
   <div class="supporters">
-  	<div id="latest_news">
-  		<ul id="latest_header" class="headlineLargeGray">
-	  		<li class="supporters_header"><span>LATEST NEWS</span></li>
-	  		<li id="follow"><span>Follow</span><a href="" id="facebook_follow"><img src="../images/facebook_follow.png"></a><a href=""><img id="google_follow" src="../images/google_follow.png"></a></li>
-  		</ul>
-  		<ul id="news">
-  			<li></li>
-  			<li></li>
-  			<li></li>
-  		</ul>
-  	</div>
-  	<div id="supporter_numbers">
-  		<ul id="google_pluses">
-	  		<li id="google_header" class="headlineLargeGray">
-		  		<span>GOOGLE+</span>
-	  		</li>
-	  		<li>
-		  		<div></div>
-		  		<div></div>
-	  		</li>
-  		</ul>
-  		
-  		<ul id="facebook_likes">
-	  		<li id="facebook_header" class="headlineLargeGray">
-		  		<span>FACEBOOK</span>
-	  		</li>
-	  		<li>
-		  		<div></div>
-		  		<div></div>
-	  		</li>
-  		</ul>
-  	</div>
-  	
-  	
-  <ul id="supporters_footer">
-	<li>
-		<div class="headlineLargeGray">
-			<img src="../images/buzz_sign.png"><span>SFSUPERBOWL SUPPORTERS</span>
+	  <div id="supporters_main">
+		<div id="latest_news">
+			<ul id="latest_header" class="headlineLargeGray">
+				<li class="supporters_header"><span>LATEST NEWS</span></li>
+				<li id="supporters_follow"><span>Follow</span><a href="" id="facebook_follow"><img src="../images/facebook_follow.png"></a><a href=""><img id="google_follow" src="../images/google_follow.png"></a></li>
+			</ul>
+			<ul id="news">
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
 		</div>
-	</li>
-	<li id="supporters_internal">
-	<p>Share you excitement for a Bay Area Super Bowl with a <em>Like</em> or a <em>+1</em>.</p>
-	</li>
+		<div id="supporter_numbers">
+			<ul id="google_pluses">
+				<li id="google_header" class="headlineLargeGray">
+					<span>GOOGLE+</span>
+					</li>
+				<li>
+					<div><a href=""><img src="../images/google_pluses.png"></a><span>+1'S</span></div>
+					<div></div>
+				</li>
+			</ul>
+		
+			<ul id="facebook_likes">
+				<li id="facebook_header" class="headlineLargeGray">
+					<span>FACEBOOK</span>
+				</li>
+				<li>
+					<div><a href=""><img src="../images/facebook_likes.png"></a><span>LIKES</span></div>
+					<div></div>
+				</li>
+			</ul>
+		</div>
+	  </div>
+  	
+	  <ul id="supporters_footer">
+		  <li>
+			  <div class="headlineLargeGray">
+				  <img src="../images/buzz_sign.png"><span>SFSUPERBOWL SUPPORTERS</span>
+			  </div>
+		  </li>
+		  <li id="supporters_internal">
+			  <p>Share you excitement for a Bay Area Super Bowl with a <em>Like</em> or a <em>+1</em>.</p>
+	      </li>
   
-	<li id="supporters_count">
-	</li>
-  </ul>
+	      <li id="supporters_count">
+	      </li>
+	  </ul>
+	</div>
 </div>
   
 <?php include('../footer.php'); ?>
