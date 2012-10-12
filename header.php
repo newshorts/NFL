66273da38cdf6b51ca43429440d6db5ae5cc4026
ssworks <twistiesbros@gmail.com>
2012-10-11 19:42:44 -0700

    PHP in Directories

--- header.php
+++ header.php
@@ -4,9 +4,9 @@
         <title></title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-        <link href="css/reset.css" type="text/css" rel="stylesheet" />
-        <link href="css/font.css" type="text/css" rel="stylesheet" />
-        <link href="css/style.css" type="text/css" rel="stylesheet" />
+        <link href="../css/reset.css" type="text/css" rel="stylesheet" />
+        <link href="../css/font.css" type="text/css" rel="stylesheet" />
+        <link href="../css/style.css" type="text/css" rel="stylesheet" />
 
     </head>
     <body>
--- instagram.php
+++
@@ -1,5 +0,0 @@
-<?php include('header.php'); ?>
-            <div id="dynamic">
-                <?php include('photos.php'); ?>
-            </div>
-<?php include('footer.php'); ?>
