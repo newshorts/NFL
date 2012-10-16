e0c23e73a270151e6c92862570ad078a15a1fd34
ssworks <twistiesbros@gmail.com>
2012-10-16 14:55:11 -0700

    intro logo LI removed

--- footer.php
+++ footer.php
@@ -5,20 +5,22 @@
  <!-- Twitter -->     
     <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
     
+    <script src="<?php echo ROOT; ?>/tweets/tweet.js" charset="utf-8"></script>
+    
     
     
 <!-- G+ -->    
     <script type="text/javascript">
-  (function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/plusone.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  })();
-</script>
+	  (function() {
+	    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+	    po.src = 'https://apis.google.com/js/plusone.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+	  })();
+	</script>
 
 
+	
 
-    <script src="<?php echo ROOT; ?>/tweets/tweet.js" charset="utf-8"></script>
     </body>
     
      
