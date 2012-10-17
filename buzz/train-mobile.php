0863381ee106e27e86c5c15d7c273479ad400c17
Russell Shearer <rgipsons@gmail.com>
2012-10-17 06:36:46 -0700

    adding styles for buzz page

--- buzz/train-mobile.php
+++ buzz/train-mobile.php
@@ -1,11 +1,10 @@
 
 <ul class="trainCar" data-pos="0">
-    <li class="trainCompartment" data-pos="">
+    <li class="trainCompartment" data-pos="" id="train_car1">
         <a href="<?php echo ROOT; ?>tweets/" class="ajaxify">
             <span class="headlineSmallGray smallBoxHeading">TWEETS</span>
             <img class="smallBoxIcon" src="../images/icon_twitter.png">
-            <hr class="smallBoxHR" />
-            <ul class="numbers_small">
+            <ul class="numbers_medium">
                 <li class="nil">0</li>
                 <li class="nil">0</li>
                 <li class="nil">0</li>
@@ -14,15 +13,14 @@
                 <li class="nil">0</li>
                 <li class="nil">0</li>
             </ul>
-            <div id="twitter_count" class=" smallNumbersOrange countOrange small"></div>
+            <div id="twitter_count" class="mediumNumbersOrange countOrange medium"></div>
         </a>
     </li>
-    <li class="trainCompartment" data-pos="">
+    <li class="trainCompartment" data-pos="" id="train_car2">
         <a href="<?php echo ROOT; ?>instagram/" class="ajaxify">
             <span class="headlineSmallGray smallBoxHeading">PHOTOS</span>
             <img class="smallBoxIcon" src="../images/icon_instagram.png">
-            <hr class="smallBoxHR" />
-            <ul class="numbers_small">
+            <ul class="numbers_medium">
                 <li class="nil">0</li>
                 <li class="nil">0</li>
                 <li class="nil">0</li>
@@ -31,15 +29,14 @@
                 <li class="nil">0</li>
                 <li class="nil">0</li>
             </ul>
-            <div id="instagram_photo_count" class="smallNumbersOrange countOrange small"></div>
+            <div id="instagram_photo_count" class="mediumNumbersOrange countOrange medium"></div>
         </a>
     </li>
-    <li class="trainCompartment" data-pos="">
+    <li class="trainCompartment" data-pos="" id="train_car3">
         <a href="<?php echo ROOT; ?>supporters/" class="ajaxify">
             <span class="headlineSmallGray smallBoxHeading">SUPPORTERS</span>
             <img class="smallBoxIconLeft" src="../images/icon_facebook.png"><img class="smallBoxIcon" src="../images/icon_google.png">
-            <hr class="smallBoxHR" />
-            <ul class="numbers_small">
+            <ul class="numbers_medium">
                 <li class="nil">0</li>
                 <li class="nil">0</li>
                 <li class="nil">0</li>
@@ -48,7 +45,7 @@
                 <li class="nil">0</li>
                 <li class="nil">0</li>
             </ul>
-            <div id="gfb_count" class="smallNumbersOrange countOrange small"></div>
+            <div id="gfb_count" class="mediumNumbersOrange countOrange medium"></div>
         </a>
     </li>
 </ul>
