e591378945a79cb23dd3401bf3ea10eff1e4ace3
Russell Shearer <rgipsons@gmail.com>
2012-10-18 12:40:43 -0700

    additional meta tag

--- header.php
+++ header.php
@@ -25,6 +25,7 @@ if(!isset($_SESSION['sfsuperbowlintro'])) {
         
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta property="og:image" content="http://www.sfsuperbowl.com/images/fb/sfsb_128x128.jpg" />
+        <link rel="image_src" href="http://www.sfsuperbowl.com/images/fb/sfsb_128x128.jpg" />
         <link rel="icon" type="image/png" href="<?php echo ROOT; ?>favicon.ico">
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="<?php echo ROOT; ?>js/lib/jquery.min.js">\x3C/script>')</script>
