5267a7b75900a954d3d5ac378f2a4f19c5dc7126
Mike Newell <mike@iwearshorts.com>
2012-10-17 23:22:52 -0700

    output.json

--- services/output.json.php
+++ services/output.json.php
@@ -3,7 +3,7 @@ $output = file_get_contents('output.json');
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
-print json_encode($output);
+echo json_encode($output);
 
 
 ?>
