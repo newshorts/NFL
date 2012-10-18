41f2a5cbbbb468cd2a0036985045feba41776220
Mike Newell <mike@iwearshorts.com>
2012-10-17 23:20:34 -0700

    json

--- services/output.json.php
+++ services/output.json.php
@@ -1,6 +1,9 @@
 <?php
+$output = file_get_contents('output.json');
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
-print file_get_contents('output.json');
+print json_encode($output);
+
+
 ?>
