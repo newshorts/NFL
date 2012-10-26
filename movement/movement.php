def4d73269a1077c0be43595831064ce468eadc2
Mike Newell <mike@iwearshorts.com>
2012-10-26 11:58:08 -0700

    fixed tracking movements

--- movement/movement.php
+++ movement/movement.php
@@ -7,7 +7,7 @@
 				</div>
 			</li>
 			<li>
-				<p>Our bid for the Super Bowl starts with you. Help us show why the Bay Area will be the perfect host for Super Bowl 50. Simply Tweet, share or post with <a onclick="_gaq.push(['_trackEvent', 'movement', 'click', 'hashtag']); return false; " href="https://twitter.com/search/realtime?q=%23SFSuperbowl&src=typd" target="_blank";>&#35;SFSuperBowl</a> to show your support.</p>
+				<p>Our bid for the Super Bowl starts with you. Help us show why the Bay Area will be the perfect host for Super Bowl 50. Simply Tweet, share or post with <a onclick="_gaq.push(['_trackEvent', 'movement', 'click', 'hashtag']);" href="https://twitter.com/search/realtime?q=%23SFSuperbowl&src=typd" target="_blank";>&#35;SFSuperBowl</a> to show your support.</p>
 			</li>
 		</ul>	
 		<ul id="movement_steps">
@@ -17,9 +17,9 @@
 				</div>
 				<div class="headline">
 					<h3 class="headlineSmallGray" id="followSF">FOLLOW @SFSUPERBOWL</h3>
-                                        <div class="right"><a onclick="_gaq.push(['_trackSocial', 'google', 'share', 'join_pluspage']); return false; " class="google link" href="https://plus.google.com/u/0/116535953378851378506/" target="_blank"><img src="../images/follow_google.png"></a></div>
-					<div class="right"><a onclick="_gaq.push(['_trackSocial', 'facebook', 'share', 'join_page']); return false; " class="facebook link" href="https://www.facebook.com/SFSuperbowl" target="_blank"><img src="../images/follow_facebook.png"></a><p id='msg'></p></div>
-					<div class="right"><a onclick="_gaq.push(['_trackSocial', 'twitter', 'share', 'join_feed']); return false; " class="twitter_link link" href="https://twitter.com/SFSuperbowl" target="_blank"><img src="../images/follow_twitter.png"></a></div>
+                                        <div class="right"><a onclick="_gaq.push(['_trackSocial', 'google', 'share', 'join_pluspage']); " class="google link" href="https://plus.google.com/u/0/116535953378851378506/" target="_blank"><img src="../images/follow_google.png"></a></div>
+					<div class="right"><a onclick="_gaq.push(['_trackSocial', 'facebook', 'share', 'join_page']);" class="facebook link" href="https://www.facebook.com/SFSuperbowl" target="_blank"><img src="../images/follow_facebook.png"></a><p id='msg'></p></div>
+					<div class="right"><a onclick="_gaq.push(['_trackSocial', 'twitter', 'share', 'join_feed']);" class="twitter_link link" href="https://twitter.com/SFSuperbowl" target="_blank"><img src="../images/follow_twitter.png"></a></div>
 				</div>
 				
 			</li>
@@ -29,8 +29,8 @@
 					<img src="../images/movement2.png">
 				</div>
 				<div class="headline">
-					<h3 class="headlineSmallGray"><a  onclick="_gaq.push(['_trackEvent', 'movement', 'click', 'logo']); return false; " href="../images/movement_poster.png" target="_blank">SHARE THE LOGO >></a></h3>
-					<p>Grow the movement. <a  onclick="_gaq.push(['_trackEvent', 'movement', 'click', 'download']); return false; " href="../posters/SFSB_Poster.png" target="_blank">Download</a> or <a onclick="_gaq.push(['_trackEvent', 'movement', 'click', 'print']); return false; " href="../posters/SFSB_Poster.pdf" target="_blank">print</a> the logo.</p>
+					<h3 class="headlineSmallGray"><a  onclick="_gaq.push(['_trackEvent', 'movement', 'click', 'logo']);" href="../images/movement_poster.png" target="_blank">SHARE THE LOGO >></a></h3>
+					<p>Grow the movement. <a  onclick="_gaq.push(['_trackEvent', 'movement', 'click', 'download']);" href="../posters/SFSB_Poster.png" target="_blank">Download</a> or <a onclick="_gaq.push(['_trackEvent', 'movement', 'click', 'print']); return false; " href="../posters/SFSB_Poster.pdf" target="_blank">print</a> the logo.</p>
 				</div>
 
 			</li>
@@ -40,8 +40,8 @@
 					<img src="../images/movement3.png">
 				</div>
 				<div class="headline">
-					<h3 class="headlineSmallGray"><a onclick="_gaq.push(['_trackEvent', 'movement', 'click', 'logo']); return false; " href="../images/movement_poster.png" target="_blank">COMING SOON: THE LIVING LOGO</a></h3>
-					<p>Use your smartphone to unlock exclusive <a onclick="_gaq.push(['_trackEvent', 'movement', 'click', 'hashtag']); return false; " href="https://twitter.com/search/realtime?q=%23SFSuperbowl&src=typd" target="_blank";>&#35;SFSuperBowl</a> content.</p>
+					<h3 class="headlineSmallGray"><a onclick="_gaq.push(['_trackEvent', 'movement', 'click', 'logo']);" href="../images/movement_poster.png" target="_blank">COMING SOON: THE LIVING LOGO</a></h3>
+					<p>Use your smartphone to unlock exclusive <a onclick="_gaq.push(['_trackEvent', 'movement', 'click', 'hashtag']);" href="https://twitter.com/search/realtime?q=%23SFSuperbowl&src=typd" target="_blank";>&#35;SFSuperBowl</a> content.</p>
 				</div>
 			</li>
 			
