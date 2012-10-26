10c24a9eb25a8b599891ef1d84c05fc0590cf837
Mike Newell <mike@iwearshorts.com>
2012-10-26 01:45:53 -0700

    added twitter follower query, we will see if this works

---
+++ services/getTwitterFollowers.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'Zend/Service/Twitter.php';
+
+$token = '850078843-its7oIEsjmvlZZHb81feiSrHdA8dXy86RkQHXXoM';
+$t = new Zend_Service_Twitter(array(
+    'username' => 'SFSuperbowl',
+    'accessToken' => $token
+));
+
+$response = $t->user->followers();
+
+echo "<pre>";
+print_r($response);
+
+?>
