803f53f75db7887c19de997811bb66eb4ef499c2
Russell Shearer <rgipsons@gmail.com>
2012-11-06 15:08:18 -0800

    adding new images for index 2005 and css

--- buzz/index.php
+++
@@ -1,6 +0,0 @@
-<?php include('../header.php'); ?>
-    <div id="dynamic">
-        <?php include('numbers.php'); ?>
-    </div>
-
-<?php include('../footer.php'); ?>
--- movement/index.php
+++
@@ -1,8 +0,0 @@
-<?php include('../header.php'); ?>
-
-    <div id="dynamic">
-        <?php include('movement.php'); ?>
-    </div>
- 
-<?php include('../footer.php'); ?>
-
