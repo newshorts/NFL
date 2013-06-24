1b47cd8e119308f4cd0bbfc2322802b6adc19968
Mike Newell <mike@iwearshorts.com>
2013-06-24 06:10:52 -0700

    added new twitter outh library to work with twitter api v 1.1

--- services/twitterTracker.php
+++ services/twitterTracker.php
@@ -2,6 +2,8 @@
 require_once 'Zend/Db.php';
 require_once 'Zend/Service/Twitter/Search.php';
 
+require_once ('codebird.php');
+
 /**
  * Zend Framework (http://framework.zend.com/)
  *
@@ -53,6 +55,9 @@ class TwitterTracker {
      * @var object
      */
     private $t;
+    
+    private $bearer_token;
+    private $cb;
     
     /**
      * Construct
@@ -108,9 +113,35 @@ class TwitterTracker {
         $this->table_name = 'tweets';
         
         // twitter
-        $this->ts   = new Zend_Service_Twitter_Search('json');
+        // DEPRECATED
+//        $this->ts   = new Zend_Service_Twitter_Search('json');
         
+        // codebird - for search
+        \Codebird\Codebird::setConsumerKey('Mr3tmdYJtDUBHFCIi0tPQw', 'yPFzdcuzEj0fc8jwuzrE4LdJhmNGnaiqlfv8aOVwGRU'); // static, see 'Using multiple Codebird instances'
+        $cb = \Codebird\Codebird::getInstance();
+        $cb->setToken('850078843-rKjQFWOzZzPSUCdEyQIJOHvQo6Yna1ohGP8El83K', 'wE6HRo7mPguFT7atpJGHURUccuW5kXYKvLa1WevS0Y');
+        $this->cb = $cb;
         
+        $file = "bearer.txt";
+        if(file_exists($file)) {
+            echo "using " . $file;
+            echo '<br />';
+            $bearer_token = file_get_contents($file);
+            echo "using b token: " . $bearer_token;
+            echo '<br />';
+            \Codebird\Codebird::setBearerToken($bearer_token);
+        } else {
+            echo "getting new bearer token";
+            echo '<br />';
+            $reply = $this->cb->oauth2_token();
+            $bearer_token = $reply->access_token;
+            echo "setting new bearer toekn to file: " . $bearer_token;
+            echo '<br />';
+            file_put_contents($file, $bearer_token);
+        }
+        
+        $this->bearer_token = $bearer_token;
+            
     }
     
     /**
@@ -175,7 +206,11 @@ class TwitterTracker {
      * @return json 
      */
     protected function searchForTag($tag) {
-        return $this->ts->search($tag, array('lang' => 'en', 'rpp' => 100));
+        
+        
+        return $this->cb->search_tweets('q=' . $tag, true);
+        // DEPRECATED
+//        return $this->ts->search($tag, array('lang' => 'en', 'rpp' => 100));
     }
     
     /**
@@ -263,9 +298,8 @@ class Tag extends TwitterTracker {
     public function search() {
 //        echo "<pre>";
         $this->results = $this->searchForTag($this->tag_name);
-//        print_r($this->results);
+//        var_dump($this->results);
         $this->parseResults();
-//        print_r($this->tweets);
         $this->checkTweets();
     }
     
@@ -275,26 +309,138 @@ class Tag extends TwitterTracker {
      * @param array $tweets an array of twitter results returned from a search 
      */	
     private function parseResults() {
+        echo '<pre>';
+        echo '<br />';
+        echo '###';
+        echo 'adding tweets to the database in the following structure';
+        echo '###';
         
-        $tweets = $this->results['results'];
+        $tweets = array();
         
-        $tweets = array_reverse($tweets);
-        
-        foreach($tweets as $tweet) {
-            echo "<pre>";
-            print_r($tweet);
-            $this->tweets[] =   array(  
-                                        'tid' => $tweet['id_str'], 
-                                        'tag_name' => $this->tag_name,
-                                        'profile_img' => $tweet['profile_image_url'],
-                                        'name' => $tweet['from_user_name'],
-                                        'username' => $tweet['from_user'],
-                                        'text' => $tweet['text']
-                                    );
+        foreach($this->results->statuses as $status) {
+            var_dump($status);
+            var_dump($status->user);
+            
+            $status = array(
+                'tid' => $status->id_str, 
+                'tag_name' => $this->tag_name,
+                'profile_img' => $status->user->profile_image_url,
+                'name' => $status->user->name,
+                'username' => $status->user->screen_name,
+                'text' => $status->text
+            );
+            
+            $tweets[] = $status;
+            
         }
+        // DEPRECATED
+//        $tweets = $this->results['results'];
+        
+        $this->tweets = array_reverse($tweets);
+//        DEPRECATED
+//        foreach($tweets as $tweet) {
+//            echo "<pre>";
+//            print_r($tweet);
+//            $this->tweets[] =   array(  
+//                                        'tid' => $tweet['id_str'], 
+//                                        'tag_name' => $this->tag_name,
+//                                        'profile_img' => $tweet['profile_image_url'],
+//                                        'name' => $tweet['from_user_name'],
+//                                        'username' => $tweet['from_user'],
+//                                        'text' => $tweet['text']
+//                                    );
+//        }
     }
     
 }
+
+//class Twitauth {
+//    var $key = '';
+//    var $secret = '';
+//
+//    var $request_token = "https://twitter.com/oauth/request_token";
+//
+//    function Twitauth($config) {
+//        $this->key = $config['key']; // consumer key from twitter
+//        $this->secret = $config['secret']; // secret from twitter
+//    }
+//
+//    function getRequestToken() {
+//        // Default params
+//        $params = array(
+//            "oauth_version" => "1.0",
+//            "oauth_nonce" => time(),
+//            "oauth_timestamp" => time(),
+//            "oauth_consumer_key" => $this->key,
+//            "oauth_signature_method" => "HMAC-SHA1"
+//         );
+//
+//         // BUILD SIGNATURE
+//            // encode params keys, values, join and then sort.
+//            $keys = $this->_urlencode_rfc3986(array_keys($params));
+//            $values = $this->_urlencode_rfc3986(array_values($params));
+//            $params = array_combine($keys, $values);
+//            uksort($params, 'strcmp');
+//
+//            // convert params to string 
+//            foreach ($params as $k => $v) {$pairs[] = $this->_urlencode_rfc3986($k).'='.$this->_urlencode_rfc3986($v);}
+//            $concatenatedParams = implode('&', $pairs);
+//
+//            // form base string (first key)
+//            $baseString= "GET&".$this->_urlencode_rfc3986($this->request_token)."&".$this->_urlencode_rfc3986($concatenatedParams);
+//            // form secret (second key)
+//            $secret = $this->_urlencode_rfc3986($this->secret)."&";
+//            // make signature and append to params
+//            $params['oauth_signature'] = $this->_urlencode_rfc3986(base64_encode(hash_hmac('sha1', $baseString, $secret, TRUE)));
+//
+//         // BUILD URL
+//            // Resort
+//            uksort($params, 'strcmp');
+//            // convert params to string 
+//            foreach ($params as $k => $v) {$urlPairs[] = $k."=".$v;}
+//            $concatenatedUrlParams = implode('&', $urlPairs);
+//            // form url
+//            $url = $this->request_token."?".$concatenatedUrlParams;
+//
+//         // Send to cURL
+//         print $this->_http($url);          
+//    }
+//
+//    function _http($url, $post_data = null) {       
+//        $ch = curl_init();
+//
+//        curl_setopt($ch, CURLOPT_URL, $url);
+//        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+//        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+//
+//        if(isset($post_data))
+//        {
+//            curl_setopt($ch, CURLOPT_POST, 1);
+//            curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+//        }
+//
+//        $response = curl_exec($ch);
+//        $this->http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+//        $this->last_api_call = $url;
+//        curl_close($ch);
+//
+//        return $response;
+//    }
+//
+//    function _urlencode_rfc3986($input) {
+//        if (is_array($input)) {
+//            return array_map(array('Twitauth', '_urlencode_rfc3986'), $input);
+//        }
+//        else if (is_scalar($input)) {
+//            return str_replace('+',' ',str_replace('%7E', '~', rawurlencode($input)));
+//        }
+//        else{
+//            return '';
+//        }
+//    }
+//}
 
 //$sfsuperbowl = new Tag('sfsuperbowl');
 //echo "<pre>";
